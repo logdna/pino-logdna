97d02ee547bb9a0d159d11e2faed22dfdb80f695
Mike Del Tito <mike.deltito@logdna.com>
2021-10-27 01:01:07 -0400

    feat!: implement pino@7 transport interface
    Pino has moved toward in-process transports via worker threads in the
    latest version. This refactors the stream implementation to work with
    this new interface. The "legacy" cli option is still available for users
    that would like to continue sending logs from a separate process.
    
    BREAKING CHANGE: This transport is now untested *as an in-process log
    destination* for `pino < 7.0.0`, and while it may work, it is not recommended
    and will no longer be officially supported. Users that wish to use the
    latest version of `pino-logdna` with earlier versions of `pino` should
    pipe program output to the transport instead. See
    https://getpino.io/#/docs/transports?id=legacy-transports
    
    Closes #3

---
+++ test/common/test-server.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const {once} = require('events')
+const http = require('http')
+const {parse} = require('querystring')
+
+module.exports = function create(test) {
+  const server = http.createServer((req, res) => {
+    const parts = req.url.split('?')
+    const query = parts[1] ? parse(parts[1]) : {}
+
+    let data = ''
+    req.on('data', (chunk) => {
+      data += chunk
+    })
+
+    req.on('end', () => {
+      test({
+        body: JSON.parse(data)
+      , query
+      })
+      res.writeHead(200)
+      res.end()
+    })
+  })
+
+  return {
+    server
+  , async listen(listen_port = 0) {
+      server.listen(listen_port)
+      await once(server, 'listening')
+
+      const {address, port} = server.address()
+      return `http://${address}:${port}`
+    }
+  , async close() {
+      server.close()
+      await once(server, 'close')
+    }
+  }
+}
